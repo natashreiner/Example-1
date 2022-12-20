79ca48069a42334ff3705bf3bbc4b3b8dc4cf1db
Nata Schreiner <natashreiner@gmail.com>
2022-12-20 23:40:52 +0700

    fixed everything!!!

--- Example005_Conditions/Program.cs
+++ Example005_Conditions/Program.cs
@@ -1,12 +1,10 @@
-﻿Console.Write("Введите имя пользователя: ");
-string username = Console.Readline();
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
 
-if(username == "Маша");
+if (username.ToLower() == "маша")
 {
     Console.WriteLine("Ура, это же Маша!");
 }
 else
-{
     Console.Write("Привет, ");
     Console.WriteLine(username);
-}
